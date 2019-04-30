f145ef31e60097a1b5c4f0bf1647842435c5e99a
ithar.malik <ithar.malik@spika.com>
2019-04-30 10:44:09 +0100

    Updated request mapping with root mapping.

--- pet-clinic-web/src/main/java/com/ithar/malik/udemy/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ithar/malik/udemy/spring/petclinic/controllers/OwnerController.java
@@ -3,10 +3,11 @@ package com.ithar.malik.udemy.spring.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "owners/index"})
+    @RequestMapping({"", "/", "/index"})
     public String list() {
 
         return "owners/list";
--- pet-clinic-web/src/main/java/com/ithar/malik/udemy/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ithar/malik/udemy/spring/petclinic/controllers/VetController.java
@@ -3,10 +3,11 @@ package com.ithar.malik.udemy.spring.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "vets/index"})
+    @RequestMapping({"", "/", "/index"})
     public String list() {
 
         return "vets/list";

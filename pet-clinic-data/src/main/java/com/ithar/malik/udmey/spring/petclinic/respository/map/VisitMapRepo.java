d9e106d69a4850770eb1b9045ffcc1ee10fcb99f
ithar.malik <ithar.malik@spika.com>
2019-05-30 11:06:29 +0100

    Added visit map repo.

---
+++ pet-clinic-data/src/main/java/com/ithar/malik/udmey/spring/petclinic/respository/map/VisitMapRepo.java
@@ -0,0 +1,8 @@
+package com.ithar.malik.udmey.spring.petclinic.respository.map;
+
+import com.ithar.malik.udmey.spring.petclinic.model.Visit;
+import com.ithar.malik.udmey.spring.petclinic.respository.VisitRepository;
+
+public class VisitMapRepo extends MapRepo<Visit, Long> implements VisitRepository<Visit, Long> {
+
+}

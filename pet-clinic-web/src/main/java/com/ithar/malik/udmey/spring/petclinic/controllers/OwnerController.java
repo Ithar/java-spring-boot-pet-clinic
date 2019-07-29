100a81f6953efb89e0c95b7d9fcc6cf6281937c6
ithar.malik <ithar.malik@spika.com>
2019-07-29 10:31:56 +0100

    Added data binding.

--- pet-clinic-web/src/main/java/com/ithar/malik/udmey/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ithar/malik/udmey/spring/petclinic/controllers/OwnerController.java
@@ -1,13 +1,17 @@
 package com.ithar.malik.udmey.spring.petclinic.controllers;
 
 import com.ithar.malik.udmey.spring.petclinic.service.OwnerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
@@ -16,6 +20,11 @@ public class OwnerController {
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    @InitBinder
+    public void securedFields(WebDataBinder webDataBinder) {
+        webDataBinder.setDisallowedFields("id"); // prevents modification of ids via forms
     }
 
     @GetMapping({"", "/", "/index", "/index.html"})
